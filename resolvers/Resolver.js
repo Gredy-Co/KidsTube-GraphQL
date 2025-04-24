65fb307e92178c8e0b38e17566ff8a28f62f2763
keiry-chacon <keirychas@gmail.com>
2025-04-24 14:32:00 -0600

    feat(graphql): update resolver for videos

--- resolvers/Resolver.js
+++ resolvers/Resolver.js
@@ -3,49 +3,50 @@ const Video = require("../models/VideoModel");
 const Playlist = require("../models/PlaylistModel");
 
 module.exports = {
-    videosByUser: async (args) => {
-        try {
-          const { userId } = args;
-    
-          if (!userId) {
-            throw new Error("userId no proporcionado");
-          }
-    
-          const videos = await Video.find({ createdBy: userId });
-    
-          if (videos.length === 0) {
-            throw new Error("No videos found for this user");
-          }
-    
-          return videos;
-        } catch (error) {
-          console.error("Error getting videos:", error);
-          throw new Error("Internal Server Error");
-        }
-      },
-    
-      playlistsByUser: async (args) => {
-        try {
-          const { userId } = args;
-    
-          if (!userId) {
-            throw new Error("userId no proporcionado");
-          }
-    
-          const playlists = await Playlist.find({ createdBy: userId })
-            .populate("associatedProfiles")
-            .populate("videos");
-    
-          if (playlists.length === 0) {
-            throw new Error("No playlists found for this user");
-          }
-    
-          return playlists;
-        } catch (error) {
-          console.error("Error getting playlists:", error);
-          throw new Error("Internal Server Error");
-        }
-      },
+  videosByUser: async (args) => {
+    try {
+      const { userId } = args;
+
+      if (!userId) {
+        throw new Error("userId no proporcionado");
+      }
+
+      const videos = await Video.find({ createdBy: userId });
+
+      if (videos.length === 0) {
+        throw new Error("No videos found for this user");
+      }
+
+      return videos;
+    } catch (error) {
+      console.error("Error getting videos:", error);
+      throw new Error("Internal Server Error");
+    }
+  },
+
+  playlistsByUser: async (args) => {
+    try {
+      const { userId } = args;
+
+      if (!userId) {
+        throw new Error("userId no proporcionado");
+      }
+
+      const playlists = await Playlist.find({ createdBy: userId })
+        .populate("associatedProfiles")
+        .populate("videos");
+
+      if (playlists.length === 0) {
+        throw new Error("No playlists found for this user");
+      }
+
+      return playlists;
+    } catch (error) {
+      console.error("Error getting playlists:", error);
+      throw new Error("Internal Server Error");
+    }
+  },
+
   profiles: async (args) => {
     try {
       const { userId } = args;
@@ -66,6 +67,7 @@ module.exports = {
       throw new Error("Internal Server Error");
     }
   },
+
   playlistsByProfile: async (args) => {
     try {
       const { profileId } = args;
